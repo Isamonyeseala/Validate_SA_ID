42024d332a224def0bfca887c24b8a475e1c8303
Isabella Monyeseala <221768963@edu.vut.ac.za>
2025-04-23 15:46:25 +0200

    Updated Test to Check for Non-Numeric Characters (RED)

--- app/src/test/java/org/example/ValidateSaIdTest.java
+++ app/src/test/java/org/example/ValidateSaIdTest.java
@@ -25,4 +25,11 @@ class ValidateSaIdTest {
     public void testValidLengthIdNumber() {
         assertTrue(ValidateSaId.isIdNumberValid("2001014800086")); // correct length
     }
+
+    @Test
+    public void testIdWithNonNumericCharacters() {
+        assertFalse(ValidateSaId.isIdNumberValid("20010A4800086")); // contains 'A'
+        assertFalse(ValidateSaId.isIdNumberValid("ABCDEFGHIJKLM")); // all letters
+        assertFalse(ValidateSaId.isIdNumberValid("20010148000@6")); // special char
+    }
 }
