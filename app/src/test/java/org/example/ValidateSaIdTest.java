13b473038ea5d9dd288a4694f6d00e1cd594fe86
Isabella Monyeseala <221768963@edu.vut.ac.za>
2025-04-24 15:01:38 +0200

    Added the Luhn test

--- app/src/test/java/org/example/ValidateSaIdTest.java
+++ app/src/test/java/org/example/ValidateSaIdTest.java
@@ -37,7 +37,7 @@ class ValidateSaIdTest {
     public void testInvalidBirthdateMonthDay() {
         assertFalse(ValidateSaId.isIdNumberValid("9913324800086"));
         assertFalse(ValidateSaId.isIdNumberValid("9902314800086"));
-        assertTrue(ValidateSaId.isIdNumberValid("9202204800086"));
+        assertFalse(ValidateSaId.isIdNumberValid("9202204800086"));
     }
 
     @Test
@@ -49,21 +49,32 @@ class ValidateSaIdTest {
 
     @Test
     public void testGenderRange() {
-        assertTrue(ValidateSaId.isIdNumberValid("9202200000086")); // Female: 0000
-        assertTrue(ValidateSaId.isIdNumberValid("9202204999086")); // Female: 4999
-        assertTrue(ValidateSaId.isIdNumberValid("9202205000086")); // Male: 5000
-        assertTrue(ValidateSaId.isIdNumberValid("9202209999086")); // Male: 9999
+        assertFalse(ValidateSaId.isIdNumberValid("9202200000086")); // Female: 0000
+        assertFalse(ValidateSaId.isIdNumberValid("9202204999080")); // Female: 4999
+        assertTrue(ValidateSaId.isIdNumberValid("9202205000089")); // Male: 5000
+        assertFalse(ValidateSaId.isIdNumberValid("9202209999086")); // Male: 9999
     }
     @Test
     public void testCitizenshipDigitValid() {
-        assertTrue(ValidateSaId.isIdNumberValid("9202205000086")); // 0 - SA citizen
-        assertTrue(ValidateSaId.isIdNumberValid("9202205000186")); // 1 - Perm resident
+        assertFalse(ValidateSaId.isIdNumberValid("9202205000086")); // 0 - SA citizen
+        assertFalse(ValidateSaId.isIdNumberValid("9202205000185")); // 1 - Perm resident
     }
 
     @Test
     public void testCitizenshipDigitInvalid() {
         assertFalse(ValidateSaId.isIdNumberValid("9202205000286")); // 2 - Invalid
         assertFalse(ValidateSaId.isIdNumberValid("9202205000986")); // 9 - Invalid
+    }
+    @Test
+    public void testValidChecksum() {
+        assertTrue(ValidateSaId.isIdNumberValid("2001014800086")); // valid checksum
+        assertTrue(ValidateSaId.isIdNumberValid("2909035800085")); // valid checksum
+    }
+
+    @Test
+    public void testInvalidChecksum() {
+        assertFalse(ValidateSaId.isIdNumberValid("2001014800080")); // invalid checksum
+        assertFalse(ValidateSaId.isIdNumberValid("2909035800080")); // invalid checksum
     }
 
 }
